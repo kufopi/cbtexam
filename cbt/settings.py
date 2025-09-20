143de1b49ca4398879670d4d33b5c0918f693aa2
kufopi <kufopo@gmail.com>
2025-09-20 11:30:28 +0100

    allowed hosts for render.com

--- cbt/settings.py
+++ cbt/settings.py
@@ -135,7 +135,7 @@ from django.core.management.utils import get_random_secret_key
 # Security
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['.railway.app', 'localhost']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost']
 
 # Database (most free hosts provide PostgreSQL)
 import dj_database_url
